d2c2bfa3bc67674088dc81da3ec7b406f2b88963
wangliu <wangliu@isoftstone.com>
2017-03-03 15:25:47 +0800

    added new files.

--- model/model.js
+++ model/model.js
@@ -1,12 +1,14 @@
 ﻿/**
  * 
- * @summary 后台对应数据模型
+ * @summary 生成ko viewmodel对应的数据描述
  */
 var dataModel = {
+    //key值为viewmodel对象的名称
     "mainViewModel": {
         title: '',
         summary: '',
-        detailList: []
+        detailList: [],
+        getData: new Function()//viewmodel中需要生成的方式
     },
     "detailViewModel": {
         detailTitle: '',
--- template/viewmodel.js
+++ template/viewmodel.js
@@ -1,5 +1,43 @@
-﻿function  ViewModel(data) {
-    var self = this;
-    self.a = ko.observable(data.a);
-    self.b = ko.observable(data.a);
-}
+﻿/**
+ * @summary 生成ViewModel的模板脚本
+ */
+var dataModel = require("../model/model.js");
+var util = require("../lib/util.js");
+
+function ViewModelTemplate() { }
+
+/**
+ * 
+ * @summary 产生ViewModel模板数据
+ */
+ViewModelTemplate.prototype.getTemplate = function () {
+    //viewmodel属性
+    var viewModelProperties = [];
+    //viewmodel包含方法
+    var viewModelFunctions = [];
+    for (var viewmodelName in dataModel) {
+        if (!dataModel.hasOwnProperty(viewmodelName))
+            continue;
+        var viewmodel = dataModel[viewmodelName];
+        viewModelProperties.push("function " + viewmodelName + "(data) {\r");
+        viewModelProperties.push("   var self = this;\r");
+        for (var viewmodelPropertyName in viewmodel) {
+            if (!viewmodel.hasOwnProperty(viewmodelPropertyName))
+                continue;
+            var propertyValue = viewmodel[viewmodelPropertyName];
+            if (util.isArray(propertyValue)) {
+                viewModelProperties.push("   self." + viewmodelPropertyName + " = ko.observableArray(data." + viewmodelPropertyName + ");\r");
+            } else if (typeof (propertyValue) == "function") {
+                viewModelFunctions.push(viewmodelName + ".prototype." + viewmodelPropertyName + "=function(){\r");
+                viewModelFunctions.push("    var self = this;\r};");
+            } else {
+                viewModelProperties.push("   self." + viewmodelPropertyName + " = ko.observable(data." + viewmodelPropertyName + ");\r");
+            }
+        }
+        viewModelProperties.push("}\r");
+    }
+
+    return viewModelProperties.join("") + "\r" + viewModelFunctions.join("");
+};
+
+module.exports = new ViewModelTemplate();
