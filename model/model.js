76aece7aa5381363f004e89a5d86f41304b6dbc3
wangliu <wangliu@isoftstone.com>
2017-03-03 10:35:52 +0800

    added view model

---
+++ model/model.js
@@ -0,0 +1,17 @@
+﻿/**
+ * 
+ * @summary 后台对应数据模型
+ */
+var dataModel = {
+    "mainViewModel": {
+        title: '',
+        summary: '',
+        detailList: []
+    },
+    "detailViewModel": {
+        detailTitle: '',
+        detailSummary: ''
+    }
+};
+
+module.exports = dataModel;
---
+++ template/viewmodel.js
@@ -0,0 +1,5 @@
+﻿function  ViewModel(data) {
+    var self = this;
+    self.a = ko.observable(data.a);
+    self.b = ko.observable(data.a);
+}
