6942ac8bf956678af995395a3d8c56da2a571c62
wangliu <wangliu@isoftstone.com>
2017-06-13 14:17:32 +0800

    updated.

--- model/model.js
+++ model/model.js
@@ -4,15 +4,21 @@
  */
 var dataModel = {
     //key值为viewmodel对象的名称
-    "mainViewModel": {
-        title: '',
-        summary: '',
-        detailList: [],
-        getData: new Function()//viewmodel中需要生成的方式
+    "MediaWorksListViewModel": {
+        userInfo: '',
+        worksList: []
     },
-    "detailViewModel": {
-        detailTitle: '',
-        detailSummary: ''
+    "MediaNewsDetail": {
+        "newsId": 10720,
+        "picCover": "http://image.bitautoimg.com/wapimg-360-240/appimage/media/20170215/w599_h400_f34ca849c95a4bedbbb6754ca3a4e710.jpg",
+        "filePath": "bitauto.yicheapp://yiche.app/medianews.xiangqing?id=10720&ts=20170216154202",
+        "title": "奥迪A6L",
+        "publishTime": "2017-02-16 15:42",
+        "commentCount": 1,
+        "lastModify": "20170216154202",
+        "type": 21,
+        "dataVersion": "6.4",
+        "Score": 0
     }
 };
 
